c0d64d29ef24688e6cdf8fb2332ec2aaf5ea0e8d
Scott Dodge <scott@goldilocks.design>
2020-03-29 16:52:06 -0500

    update Blobs to regenerate on minute change

--- src/components/Blob.js
+++ src/components/Blob.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSpring, animated } from 'react-spring'
 import { useWindowSize } from "@reach/window-size"
 import blobs from 'blobs'
+import moment from 'moment'
 
 export default ({ size, complexity, contrast, gradient, cx, cy }) => {
   const { width, height } = useWindowSize()
+  const [ time, setTime ] = useState(moment().format())
   const length = width > height ? width : height
   const config = {
     mass: size / length * 400,
@@ -12,6 +14,7 @@ export default ({ size, complexity, contrast, gradient, cx, cy }) => {
     friction: 250,
   }
   
+  // blobs returns an entire SVG structure, we just want the d from the path
   const randBlob = () => blobs.editable({
     size,
     complexity,
@@ -21,6 +24,19 @@ export default ({ size, complexity, contrast, gradient, cx, cy }) => {
   }).children[0].children[0].attributes.d
 
   const [{ shape }, set] = useSpring(() => ({ shape: randBlob(), config }))
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const previousMinute = moment(time).minute()
+      const currentMinute = moment().minute()
+      const isNewMinute = previousMinute !== currentMinute
+
+      if (isNewMinute) {
+        setTime(moment().format())
+      }
+    }, 1000)
+    return () => clearInterval(timer)
+  })
 
   set({shape: randBlob()})
 
