1f3c812388064302b7e6bddaca8067769554dcd7
Scott Dodge <scott@goldilocks.design>
2020-04-06 17:59:14 -0500

    add web app router

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import ReactGA from 'react-ga'
+import { Router } from "@reach/router"
 
 import Layout from 'extension/src/components/Layout'
-import Time from 'extension/src/components/Time'
 import theme from 'extension/src/theme'
 import { ContextProvider } from "extension/src/components/Context"
 
-import Notifications from './Notifications'
+import Home from '../pages/Home'
+import NotFound from '../pages/NotFound'
+import { About, Privacy } from '../pages'
 
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS)
 ReactGA.pageview(window.location.pathname + window.location.search)
@@ -16,8 +18,12 @@ export default () => (
   <ThemeProvider theme={theme}>
     <ContextProvider>
       <Layout>
-        <Time />
-        <Notifications />
+        <Router>
+          <Home path="/" />
+          <About path="about" />
+          <Privacy path="privacy" />
+          <NotFound default />
+        </Router>
       </Layout>
     </ContextProvider>
   </ThemeProvider>
